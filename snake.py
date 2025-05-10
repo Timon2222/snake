8fb19d316315236c46cd9a2d65b53955ba42ef05
Timon2222 <psakk1062@gmail.com>
2025-05-10 18:47:59 +0500

    Add files via upload

--- snake.py
+++ snake.py
@@ -21,51 +21,59 @@ class GameSprite(sprite.Sprite):
 
 
 
-class Player(GameSprite):
+class Tail(GameSprite):
     def __init__(self, player_image,  player_x, player_y,  size_x, size_y, player_speed):
         super().__init__(player_image,  player_x, player_y,  size_x, size_y, player_speed)
-        self.speed_x = player_speed
-        self.speed_y = 0
+        self.timer = timer
+
     
+
+        
     def update(self):
         self.rect.y = self.speed_y
         self.rect.x = self.speed_x
 
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_LEFT]  
-            self.rect.x -= self.speed
+            if keys_pressed[K_LEFT]  
+                self.rect.x -= self.speed
 
-        if keys_pressed[K_RIGHT]  
-            self.rect.x += self.speed
+            if keys_pressed[K_RIGHT]  
+                self.rect.x += self.speed
 
-        if keys_pressed[K_UP]  
-            self.rect.y -= self.speed
+            if keys_pressed[K_UP]  
+                self.rect.y -= self.speed
 
-        if keys_pressed[K_DOWN]  
-            self.rect.y += self.speed
-
-
+            if keys_pressed[K_DOWN]  
+                self.rect.y += self.speed
 
 
 
-
-
-
-snake = Player('snake.png', 400, 400, 60, 80, 10)
-
+snake('snake.png',300, 400, 50, 50, 50)
+food('apple.png', 250, 250, 50, 50, 50)
 game = True
 finish = False
 
+
+tails = sprite.Group()
+score = 0
 
 while game:
     if finish != True:
         window.blit(background,(0, 0))
         snake.update()
         snake.reset()
-
+        if player.colliderect(food):
+            score += 1
+            food.rect.x = randint(0, 23) * 30
+            food.rect.y = randint(0, 16) * 30
+        t = Tail('snake.png', player.rect.x-player.speed_x, player.rect.y-player.speed_y, 29, 29, 0, score)
+        tails.add(t)
+        tails.update()
+        tails.draw(window)
+        wait = 30
 
         keys= key.get_pressed()
-            if keys_pressed[K_LEFT]
+        if keys_pressed[K_LEFT]
             player.speed_x = -player_speed
             player_speed_y = 0
         if keys_pressed[K_RIGHT]  
@@ -83,6 +91,6 @@ while game:
         for e in event.get():
             if e.type == QUIT:
                 game = False    
-            
+
     clock.tick(FPS)
     display.update()
